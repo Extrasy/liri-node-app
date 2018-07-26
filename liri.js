a78b2282b9c17214d90d7e8a8acb5615a88a4ce7
Extrasy <luisalexpm@gmail.com>
2018-07-26 11:48:38 -0700

    file progress

--- liri.js
+++ liri.js
@@ -1,8 +1,22 @@
 require("dotenv").config();
+var fs = require("fs");
 
-var keys = require('keys.js');
+var keys = require('../liri-node-app/keys');
 var Spotify = require('node-spotify-api');
 var Twitter = require('twitter');
+var twitter = keys.twitter;
+var twitterclient = new Twitter(twitter);
 
+
+fs.readFile("keys.js", "utf8", function(error, data) { if (error) {
+    return console.log(error);
+}
+console.log(data);
+
+var dataArr = data;
+ // console.log(dataArr);
+
+});
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
