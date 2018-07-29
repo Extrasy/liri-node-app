4c6c28d7dbc01aab27d61be527ea7c9556e7224b
Extrasy <luisalexpm@gmail.com>
2018-07-28 22:40:54 -0700

    tweets and spotify

--- liri.js
+++ liri.js
@@ -4,19 +4,45 @@ var fs = require("fs");
 var keys = require('../liri-node-app/keys');
 var Spotify = require('node-spotify-api');
 var Twitter = require('twitter');
-var twitter = keys.twitter;
-var twitterclient = new Twitter(twitter);
+var twitterKeys = keys.twitter;
+
+var spot = (name) => {
+    var spotify = new Spotify(keys.spotify);
+    spotify.search({ type: 'track', query: name.replace(' ', '-'), limit: 2}, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+                
+        console.log("'" + name + "'");
+        console.log(data.tracks.items[0].artists[0].name);
+        console.log(data.tracks.items[0].album.name);
+        console.log(data.tracks.items[0].preview_url);
+      });
+    }
 
 
-fs.readFile("keys.js", "utf8", function(error, data) { if (error) {
-    return console.log(error);
+var meTweets = ()=>{
+var client = new Twitter(twitterKeys);
+   
+  var params = {screen_name: 'nodejs'};
+  client.get('statuses/user_timeline', params, function(error, json) {
+    if (!error) {
+      var tweets= json;
+      tweets.forEach(ele => {
+         console.log(ele.text+"\n");
+         
+      });
+    }
+  });
 }
-console.log(data);
 
-var dataArr = data;
- // console.log(dataArr);
 
-});
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
 
+
+if (process.argv[2] == 'my-Tweets') {
+    meTweets();
+    
+}else if(process.argv[2] == 'spotify-this-song'){
+    spot(process.argv[3]);
+
+}
